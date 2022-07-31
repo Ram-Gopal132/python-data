1ad8f31af6a7337f9ca26792a4a6dcebfbf4bc60
Ram Gopal <ramgopalgupta053@gmail.com>
2022-07-31 14:01:15 +0530

    python data

---
+++ datatype.py
@@ -0,0 +1,68 @@
+x=123
+y=433
+x1=-545
+x3=34234.424
+x2=-5354
+x4=87.7e100
+x5=-54.85
+
+#string
+d='bruce wayne'
+d1='yellpw'
+d2='institute'
+
+#none datatype
+m=None
+
+print(x5,type(x5))
+print(x,type(x))
+print(y,type(y))
+print(x1,type(x1))
+print(x2,type(x2))
+print(x3,type(x3))
+print(x4,type(x4))
+print(d1,type(d1))
+print(d2,type(d2))
+print(d,type(d))
+print(m,type(m))
+
+# list datatype
+# mixed datatype stored
+# update value
+
+
+a=[5,7,8,9,2,7]
+vals=[1,2.2,'python']
+colors=['red','green','orange']
+print(a,type(a))
+print(vals,type(vals))
+print(colors,type(colors))
+
+# Tuples
+
+t=(5,7,8,9,2,7)
+vals1=(1,2.2,'python')
+colors1=('red','green','orange')
+print(t,type(t))
+print(vals1,type(vals1))
+print(colors1,type(colors1))
+
+#sets datatype
+
+s1={5,7,8,9,2,7}
+val2={1,2.2,'python'}
+colors2={'red','green','orange'}
+print(s1,type(s1))
+print(val2,type(val2))
+print(colors2,type(colors2))
+
+
+#dictionary datatype
+
+car_info={
+    "brand":"ford",
+    "model":"musting",
+    "year":"2022"
+}
+print(car_info,type(car_info))
+
