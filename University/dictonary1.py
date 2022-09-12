6e215d2afdd8734a5a16ece1caa6070e3076d52e
Ram-Gopal132 <ramgopalgupta053@gmail.com>
2022-09-12 20:56:43 +0530

    py

---
+++ University/dictonary1.py
@@ -0,0 +1,8 @@
+# collection of key and value is known AS dictonary
+dic={"name":"ram","marks":90,"cource":"mca","age":22 }
+print(dic)
+for i in dic:
+    print(i,end="  ")  # only key print
+    print(dic[i] ) # key and value
+    
+    
---
+++ University/sets1.py
@@ -0,0 +1,20 @@
+day={"monday","tuesday","wedenesday"}
+day.remove("monday")
+print(day)
+
+#day.remove("ram")   # key error
+day.discard("ram")
+print(day)
+
+day1={"friday","saturday","sunday","monday","tuesday","ram"}
+print(day|day1)  #union
+
+print(day&day1) #intersection
+
+print(day-day1) # difference
+print(day1-day) #difference
+a={5,8,9,58,5,8,5,8,5}   
+b={9,858,85,8,9,5}
+print(a^b)   # Symmetric differenc jo dono mai common n ho
+print(a>b)   # commparison number of element
+print(a>b)
