f8cbe4172072f104c902843f850a9a031dfecfbf
Ram-Gopal132 <ramgopalgupta053@gmail.com>
2022-09-21 22:51:04 +0530

    r

--- University/sets1.py
+++ University/sets1.py
@@ -17,4 +17,4 @@ a={5,8,9,58,5,8,5,8,5}
 b={9,858,85,8,9,5}
 print(a^b)   # Symmetric differenc jo dono mai common n ho
 print(a>b)   # commparison number of element
-print(a>b)
+print(a<b)
---
+++ lab file/lab1.py
@@ -0,0 +1,54 @@
+a=12
+b=45
+print(a+b)
+a1=int(input("enter your number first: "))
+a2=int(input("enter your number second: "))
+print(a1+a2)
+
+name=input("Enter your name: ")
+print(name)
+
+#arithmatic operator
+b1=15
+b2=10
+print(b1+b2)
+print(b1-b2)
+print(b1*b2)
+print(b1/b2)
+print(b1**b2)
+print(b1//b2)
+print(b1%b2)
+
+# comparison operator
+
+x1=45
+x2=21
+print(x1<x2)
+print(x1<=x2)
+print(x1>=x2)
+print(x1<x2)
+print(x1==x2)
+print(x1!=x2)
+
+#identity operator
+name1="ramgopal"
+print("r" is name1)
+print("a" is not name1)
+print("u" is name1)
+
+#membership operator
+a22={2,34,5,7,87}
+
+print(2 in a22)
+print(2 not in a22)
+print(34 in a22)
+
+# bitwise operator
+num1=14
+num2=15
+print("& opr",num1 & num2)  #ANd
+print("| opr",num1 | num2)  #or
+print("^ opr",num1 ^ num2)  #xor    
+print("~ opr",~num1)  #negation
+print("<< opr",num1 << num2)    #shift left by 1 bit
+print("& opr",num1 >> num2) #shift right by 1 bit
